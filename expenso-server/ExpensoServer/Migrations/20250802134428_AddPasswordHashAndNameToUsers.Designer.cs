674bf678f8d94ba90ba7e659c5abfde559b5e346
Oleksii Palamarchuk <oleksiypalamarchuck@gmail.com>
2025-08-02 16:47:27 +0300

    Add Name and PasswordHash columns to Users table and update model snapshot

--- expenso-server/ExpensoServer/Migrations/20250802134428_AddPasswordHashAndNameToUsers.Designer.cs
+++ expenso-server/ExpensoServer/Migrations/20250802134428_AddPasswordHashAndNameToUsers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ExpensoServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ExpensoServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250802134428_AddPasswordHashAndNameToUsers")]
+    partial class AddPasswordHashAndNameToUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
